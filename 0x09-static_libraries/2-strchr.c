b3fac7a20d4e828f343eebdd16c6c521172ee67a
mangochops <133221712+mangochops@users.noreply.github.com>
2023-11-18 17:11:17 +0300

    Update 2-strchr.c

--- 0x09-static_libraries/2-strchr.c
+++ 0x09-static_libraries/2-strchr.c
@@ -1,28 +1,24 @@
 #include "main.h"
 
 /**
- * _strchr - Entry point
- * @s: input
- * @c: input
- * Return: Always 0 (Success)
+ * _strchr - Locates a character in a string.
+ * @s: string.
+ * @c: character.
+ *
+ * Return: s + index and \n.
  */
-
 char *_strchr(char *s, char c)
-
 {
+	int index = 0;
 
-int i = 0;
+	while (s[index] >= '\0')
+	{
+		if (s[index] == c)
+		{
+			return (s + index);
+		}
+		index++;
+	}
 
-for (; s[i] >= '\0'; i++)
-
-{
-
-if (s[i] == c)
-
-return (&s[i]);
-
-}
-
-return (0);
-
+	return ('\0');
 }
