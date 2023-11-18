b13eb46e869ac51f643eed80b62770905a24df7e
mangochops <133221712+mangochops@users.noreply.github.com>
2023-11-18 15:47:55 +0300

    Update 0-strcat.c

--- 0x09-static_libraries/0-strcat.c
+++ 0x09-static_libraries/0-strcat.c
@@ -26,65 +26,35 @@
 
 
 char *_strcat(char *dest, char *src)
+{
 
+int a;
+int b;
+
+i = 0;
+while (dest[i] != '\0')
 
 {
 
-
-        	int a;
-
-
-        	int b;
-
-
- 
-
-
-        	a = 0;
-
-
-        	while (dest[a] != '\0')
-
-
-        	{
-
-
-                    	a++;
-
-
-        	}
-
-
-        	b = 0;
-
-
-        	while (src[b] != '\0')
-
-
-        	{
-
-
-                    	dest[a] = src[b];
-
-
-                    	a++;
-
-
-                    	b++;
-
-
-        	}
-
-
- 
-
-
-        	dest[a] = '\0';
-
-
-        	return (dest);
-
+a++;
 
 }
 
+b = 0;
+
+while (src[b] != '\0')
+
+{
+
+dest[i] = src[b];
+
+b++;
+
+}
+
+dest[i] = '\0';
+
+return (dest);
+
+}
 
