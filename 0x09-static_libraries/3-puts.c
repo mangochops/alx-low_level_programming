23569aa484e41feae4c1093e5be09902e4e07ce8
mangochops <133221712+mangochops@users.noreply.github.com>
2023-11-18 15:55:29 +0300

    Update 3-puts.c

--- 0x09-static_libraries/3-puts.c
+++ 0x09-static_libraries/3-puts.c
@@ -1,14 +1,21 @@
 #include "main.h"
+
 void _puts(char *str)
+
 #include<stdio.h>
+
 /**
  * main - Entry point
- *
+ *_puts - exit point
  * Return: Always 0 (Success)
  */
-int main(void)
-{
-        	puts("\"Programming is like building a multilingual puzzle");
-        	return (0);
-}
 
+int main(void)
+
+{
+
+puts("\"Programming is like building a multilingual puzzle");
+
+return (0);
+
+}
