cf7a8fcd1546dad6805df60c8f6f99d7ac9a2983
mangochops <133221712+mangochops@users.noreply.github.com>
2023-11-18 17:12:44 +0300

    Update 2-strncpy.c

--- 0x09-static_libraries/2-strncpy.c
+++ 0x09-static_libraries/2-strncpy.c
@@ -1,38 +1,27 @@
 #include "main.h"
 
 /**
- * _strncpy - copy a string
- * @dest: input value
- * @src: input value
- * @n: input value
- * Return: dest
+ * _strncpy - cpoies n bytes of src to the dest string
+ * @dest: character
+ * @src: character
+ * @n: integer
+ *
+ * Return: address of dest
  */
 char *_strncpy(char *dest, char *src, int n)
-
 {
+	int i;
 
-int j;
-
-j = 0;
-
-while (j < n && src[j] != '\0')
-
-{
-
-dest[j] = src[j];
-j++;
-
-}
-
-while (j < n)
-
-{
-
-dest[j] = '\0';
-j++;
-
-}
-
-return (dest);
-
+	i = 0;
+	while (i < n && *(src + i))
+	{
+		*(dest + i) = *(src + i);
+		i++;
+	}
+	while (i < n)
+	{
+		*(dest + i) = '\0';
+		i++;
+	}
+	return (dest);
 }
