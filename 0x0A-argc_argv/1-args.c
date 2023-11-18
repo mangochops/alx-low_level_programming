8e0552326d7aa26796e8239001f681887a31a059
mangochops <133221712+mangochops@users.noreply.github.com>
2023-11-18 16:58:08 +0300

    Update 1-args.c

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,17 +1,15 @@
 #include <stdio.h>
-#include "main.h"
 
 /**
- * main - print the number of arguments passed to the program
- * @argc: number of arguments
- * @argv: array of arguments
- * Return: Always 0 (Success)
+ * main - function thats outputs the string
+ * @argc: integer
+ * @argv: character
+ * Return: 0
  */
 
-int main(int argc, char *argv[])
+int main(int argc, char __attribute__((unused)) *argv[])
 {
-(void) argv; /*Ignore argv*/
 	printf("%d\n", argc - 1);
-
 	return (0);
 }
+
