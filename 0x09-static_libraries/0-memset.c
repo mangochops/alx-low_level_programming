6e277391804e9c80bc29a3591967ca76136268cf
mangochops <133221712+mangochops@users.noreply.github.com>
2023-11-18 15:46:29 +0300

    Update 0-memset.c

--- 0x09-static_libraries/0-memset.c
+++ 0x09-static_libraries/0-memset.c
@@ -32,30 +32,20 @@ char *_memset(char *s, char b, unsigned int n)
 
 
 {
+int a = 0;
+
+for (; n > 0; a++)
+
+{
+
+s[a] = b;
+
+n--;
+
+}
 
 
-   	int a = 0;
-
-
- 
-
-
-   	for (; n > 0; a++)
-
-
-   	{
-
-
-          	s[a] = b;
-
-
-          	n--;
-
-
-   	}
-
-
-   	return (s);
+return (s);
 
 
 }
